cff5c6e9f56b8a1d5c1c299af955f4601dc873da
Ildi Xhaferri <ildi.xhaferri@fti.edu.al>
2019-01-16 21:42:49 +0100

    Inital Commit

---
+++ src/main/java/com/pluralsight/service/GoalServiceImpl.java
@@ -0,0 +1,21 @@
+package com.pluralsight.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pluralsight.model.Goal;
+import com.pluralsight.repository.GoalRepository;
+
+@Service("goalService")
+public class GoalServiceImpl implements GoalService {
+
+	
+	@Autowired
+	private GoalRepository goalRepository;
+	
+	public Goal save(Goal goal) {
+	
+		return goalRepository.save(goal);
+	}
+
+}
